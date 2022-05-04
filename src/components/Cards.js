976162472afa52f5b135d2dc9cb7fbaada46e9b6
Liav <oneandonlyliav@gmail.com>
2022-05-04 03:06:07 -0400

    implemented card shuffling

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,35 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Cards = () => {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
+  //Fisher-Yates Shuffle, courtesy of stackoverflow
+  const shuffleCards = () => {
+    const cardArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let currentIndex = cardArray.length,
+      randomIndex;
+
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [cardArray[currentIndex], cardArray[randomIndex]] = [
+        cardArray[randomIndex],
+        cardArray[currentIndex],
+      ];
+    }
+
+    return setCards(cardArray);
+  };
+
   return (
     <div className="cards">
-      <div>
-        <p>1</p>
-      </div>
-      <div>
-        <p>2</p>
-      </div>
-      <div>
-        <p>3</p>
-      </div>
-      <div>
-        <p>4</p>
-      </div>
-      <div>
-        <p>5</p>
-      </div>
-      <div>
-        <p>6</p>
-      </div>
-      <div>
-        <p>7</p>
-      </div>
-      <div>
-        <p>8</p>
-      </div>
-      <div>
-        <p>9</p>
-      </div>
+      {cards.map((card) => (
+        <div key={card} onClick={shuffleCards}>
+          <p>{card}</p>
+        </div>
+      ))}
     </div>
   );
 };
