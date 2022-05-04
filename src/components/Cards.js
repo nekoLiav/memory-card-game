dc5209984a72809bfeab8e4fdaeb71556a656806
Liav <oneandonlyliav@gmail.com>
2022-05-04 01:59:23 -0400

    added numbers to cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,18 +3,33 @@ import React from "react";
 const Cards = () => {
   return (
     <div className="cards">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div>
+        <p>1</p>
+      </div>
+      <div>
+        <p>2</p>
+      </div>
+      <div>
+        <p>3</p>
+      </div>
+      <div>
+        <p>4</p>
+      </div>
+      <div>
+        <p>5</p>
+      </div>
+      <div>
+        <p>6</p>
+      </div>
+      <div>
+        <p>7</p>
+      </div>
+      <div>
+        <p>8</p>
+      </div>
+      <div>
+        <p>9</p>
+      </div>
     </div>
   );
 };
