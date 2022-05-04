76fbbc02beecc1dfbac18347e677753c37e4a1d0
Liav <oneandonlyliav@gmail.com>
2022-05-04 01:21:30 -0400

    responsive html/css skeleton created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Cards from "./components/Cards";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <p>Memory Card Game</p>
+      </header>
+      <div className="main">
+        <div className="card-container">
+          <Cards />
+        </div>
+        <div className="score-container"></div>
+      </div>
+      <footer></footer>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Cards.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Cards = () => {
+  return (
+    <div className="cards">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Cards;
